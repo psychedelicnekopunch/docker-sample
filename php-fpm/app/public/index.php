cab7a15ae7850aa0bcd39f2d94f552f72488deb6
masa <psychedelic.nekopunch@gmail.com>
2021-02-03 16:00:43 +0900

    add

---
+++ php-fpm/app/public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>docker php-fpm sample</title>
+	</head>
+	<body>
+		hello world<br>
+		docker php-fpm sample<br>
+		<?php phpinfo(); ?>
+	</body>
+</html>
