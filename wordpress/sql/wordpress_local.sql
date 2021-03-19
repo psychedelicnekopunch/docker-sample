df6bcd2936d332b8aa5e632166ce57c760975118
masa <psychedelic.nekopunch@gmail.com>
2021-03-19 16:56:04 +0900

    update

---
+++ wordpress/sql/wordpress_local.sql
@@ -0,0 +1,2 @@
+UPDATE `wp_options` SET `option_value` = 'http://localhost:8080' WHERE `wp_options`.`option_id` = 1;
+UPDATE `wp_options` SET `option_value` = 'http://localhost:8080' WHERE `wp_options`.`option_id` = 2;
